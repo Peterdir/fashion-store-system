b561b1180b8883239df4953224f4c3b6734a39e1
Lam Khanh Duy <lamkhanhduy2k5@gmail.com>
2026-02-02 21:10:08 +0700

    Initial commit

---
+++ src/main/java/org/example/fashionstoresystem/FashionStoreSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.fashionstoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionStoreSystemApplication.class, args);
+	}
+
+}
