c10dba1da4ad4188f130f8f31598244db31fe37e
Lam Khanh Duy <lamkhanhduy2k5@gmail.com>
2026-03-24 11:06:23 +0700

    Feat: Order Management Service

--- src/main/java/org/example/fashionstoresystem/service/order/OrderManagementServiceImpl.java
+++ src/main/java/org/example/fashionstoresystem/service/order/OrderManagementServiceImpl.java
@@ -2,50 +2,40 @@ package org.example.fashionstoresystem.service.order;
 
 import lombok.RequiredArgsConstructor;
 import org.example.fashionstoresystem.entity.enums.OrderStatus;
-import org.example.fashionstoresystem.entity.jpa.Order;
 import org.example.fashionstoresystem.entity.jpa.OrderHistory;
+import org.example.fashionstoresystem.entity.jpa.OrderItem;
 import org.example.fashionstoresystem.repository.OrderHistoryRepository;
-import org.example.fashionstoresystem.repository.OrderRepository;
+import org.example.fashionstoresystem.repository.OrderItemRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderManagementServiceImpl implements OrderManagementService {
 
-    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
     private final OrderHistoryRepository historyRepository;
 
     @Override
-    public List<Order> getAllOrders() {
-        return orderRepository.findAll();
-    }
-
-    @Override
-    public Order getOrderDetails(Long id) {
-        return orderRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Đơn hàng không tồn tại!"));
-    }
-
-    @Override
     @Transactional
-    public void updateOrderStatus(Long id, OrderStatus newStatus) {
-        Order order = getOrderDetails(id);
-        OrderStatus currentStatus = order.getStatus();
+    public void updateOrderItemStatus(Long orderItemId, OrderStatus newStatus) {
+        OrderItem item = orderItemRepository.findById(orderItemId)
+                .orElseThrow(() -> new RuntimeException("Sản phẩm trong đơn hàng không tồn tại!"));
 
+        OrderStatus currentStatus = item.getStatus();
         checkStatusTransition(currentStatus, newStatus);
 
-        order.setStatus(newStatus);
-        orderRepository.save(order);
+        item.setStatus(newStatus);
+        orderItemRepository.save(item);
 
-        OrderHistory history = new OrderHistory();
-        history.setOrder(order);
-        history.setPreviousStatus(currentStatus);
-        history.setNewStatus(newStatus);
-        history.setChangeDate(new Date());
+        OrderHistory history = OrderHistory.builder()
+                .orderItem(item)
+                .previousStatus(currentStatus)
+                .newStatus(newStatus)
+                .changeDate(new Date())
+                .build();
         historyRepository.save(history);
     }
 
