92a3cbe5b4d377794025d450843267980106414c
Lam Khanh Duy <lamkhanhduy2k5@gmail.com>
2026-03-25 08:05:48 +0700

    Feat: Admin Return Request Controller

---
+++ src/main/java/org/example/fashionstoresystem/controller/AdminReturnRequestController.java
@@ -0,0 +1,44 @@
+package org.example.fashionstoresystem.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.fashionstoresystem.dto.request.ProcessReturnRequestDTO;
+import org.example.fashionstoresystem.dto.response.MessageResponseDTO;
+import org.example.fashionstoresystem.dto.response.ReturnRequestResponseDTO;
+import org.example.fashionstoresystem.service.return_request.ReturnRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/return-requests")
+@RequiredArgsConstructor
+public class AdminReturnRequestController {
+
+    private final ReturnRequestService returnRequestService;
+
+    // LẤY TẤT CẢ YÊU CẦU
+    @GetMapping
+    public ResponseEntity<List<ReturnRequestResponseDTO>> getAllReturnRequests() {
+        return ResponseEntity.ok(returnRequestService.getAllReturnRequests());
+    }
+
+    // XEM CHI TIẾT
+    @GetMapping("/{requestId}")
+    public ResponseEntity<ReturnRequestResponseDTO> getReturnRequestDetail(
+            @PathVariable Long requestId
+    ) {
+        return ResponseEntity.ok(returnRequestService.getReturnRequestDetail(requestId));
+    }
+
+    // XỬ LÝ YÊU CẦU
+    @PostMapping("/{requestId}/process")
+    public ResponseEntity<MessageResponseDTO> processReturnRequest(
+            @PathVariable Long requestId,
+            @RequestBody ProcessReturnRequestDTO dto
+    ) {
+        return ResponseEntity.ok(
+                returnRequestService.processReturnRequest(requestId, dto)
+        );
+    }
+}
