799dbdc3aac2568942279a738ebbdc940b6dab5c
Lam Khanh Duy <lamkhanhduy2k5@gmail.com>
2026-03-26 14:14:57 +0700

    Implement custome validator

---
+++ src/main/java/org/example/fashionstoresystem/validation/PasswordMatch.java
@@ -0,0 +1,17 @@
+package org.example.fashionstoresystem.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordMatchValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordMatch {
+    String message() default "Mật khẩu xác nhận không khớp";
+    String passwordField();
+    String confirmPasswordField();
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
